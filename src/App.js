99462be06b56e272fbd65722aea02000474a9d4e
ConradWilliam <mcwilliam131@gmail.com>
2023-04-07 13:22:07 +0300

    Changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Home from "./routes/Home";
 import Login from "./routes/Login";
 import SignUp from "./routes/SignUp";
 import About from "./routes/About";
-import Landlord from "./routes/Landlord";
-import BookNow from "./routes/BookNow";
+
 import "./index.css";
 
 export default function App() {
@@ -16,8 +15,8 @@ export default function App() {
         <Route path="/Login" element={<Login/>}/>
         <Route path="/SignUp" element={<SignUp/>}/>
         <Route path="/About" element={<About/>}/>
-        <Route path="/Landlord" element={<Landlord/>}/>
-        <Route path="/BookNow" element={<BookNow/>}/>
+
+
       </Routes>
       </div>
    
