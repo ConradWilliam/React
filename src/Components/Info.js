7a076d2eb5a0582f96ee079afc3abcd42b3debc6
ConradWilliam <mcwilliam131@gmail.com>
2023-04-07 12:37:14 +0300

    Changes

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Info.css';
-import { FaSearch, FaBook, FaSuitcase, FaCouch } from 'react-icons/fa';
+
 
 const Info= () => {
   return (
     <div className="about-container" style={{ backgroundColor: "#43570f" }}>
       <h1 style={{ color: "#ffffff" }}>Fun Fact about Developers</h1>
       <p style={{ color: "#ffffff" }}>Why do programmers prefer dark mode?
-      background-color:  #43570f;
+      
 Because light attracts bugs.</p>
     </div>
   );
